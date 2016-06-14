a1dafd9c8cc69080ab7f427d67c5f1a3739ebb5f
Tao <Tao>
2016-06-14 17:30:07 +0800

    Initial commit

---
+++ app/src/main/java/com/codeblog/ActLogin.java
@@ -0,0 +1,13 @@
+package com.codeblog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ui_act_login_view);
+    }
+}
