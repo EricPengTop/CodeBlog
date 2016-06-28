7d29c7869537c4d0ef3e295fdd39920fd00ccf98
Tao <Tao>
2016-06-28 17:47:16 +0800

    -modify

--- CodeBlogWeb/CBApp/views.py
+++ CodeBlogWeb/CBApp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+import json
+from django.http.response import HttpResponse
+
 
 # Create your views here.
+def get_config(request):
+    if request.method == 'GET':
+        client_type = request.GET.get('client_type', None)
+        if client_type == 'android':
+            context = {'init_image': request.get_host() + '/static/image/welcome.jpg'}
+            return HttpResponse(json.dumps(context), content_type="application/json")
+        return HttpResponse(client_type)
--- CodeBlogWeb/CodeBlogWeb/urls.py
+++ CodeBlogWeb/CodeBlogWeb/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+import CBApp.views as views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^get_config/$', views.get_config),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
